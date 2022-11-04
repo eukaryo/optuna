cdd508f4d4c40f7c2ae5ac8b99f9403aee69032e
c-bata <contact@c-bata.link>
2022-11-04 17:02:12 +0900

    Add thread-local trials cache

--- optuna/study/study.py
+++ optuna/study/study.py
@@ -55,6 +55,7 @@ _logger = logging.get_logger(__name__)
 
 class _ThreadLocalStudyAttribute(threading.local):
     in_optimize_loop: bool = False
+    cached_all_trials: Optional[List['FrozenTrial']] = None
 
 
 class Study:
@@ -226,6 +227,7 @@ class Study:
         self,
         deepcopy: bool = True,
         states: Optional[Container[TrialState]] = None,
+        use_cache: bool = False,
     ) -> List[FrozenTrial]:
         """Return all trials in the study.
 
@@ -258,10 +260,17 @@ class Study:
                 the study may corrupt and unexpected behavior may happen.
             states:
                 Trial states to filter on. If :obj:`None`, include all states.
+            use_cache:
+                If True, returns trials from an in-memory cache, which is cached
+                before evaluating the objective function.
 
         Returns:
             A list of :class:`~optuna.trial.FrozenTrial` objects.
         """
+        if use_cache and self._thread_local.cached_all_trials is not None:
+            filtered_trials = [t for t in self._thread_local.cached_all_trials if t.state in states]
+            return copy.deepcopy(filtered_trials) if deepcopy else filtered_trials
+
         if isinstance(self._storage, _CachedStorage):
             self._storage.read_trials_from_remote_storage(self._study_id)
 
